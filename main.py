203be6dcf4f444be8c0b5a77cbff4cd0128bb8d1
rahulagowda004 <rahulgowda277@gmail.com>
2024-04-25 12:10:00 +0530

    data ingestion commit

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from src.Facial_Review_System import logger
-logger.info("hi im rahul")
+from Facial_Review_System import logger
+from Facial_Review_System.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e

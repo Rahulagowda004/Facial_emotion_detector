e3ef2d4136a0c39bcc176c3a5e1500646fbe20f8
rahulagowda004 <rahulgowda277@gmail.com>
2024-04-25 11:32:40 +0530

    error commit

--- src/Facial_Review_System/utils/common.py
+++ src/Facial_Review_System/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.Facial_Review_System import logger
+from Facial_Review_System import logger
 import json
 import joblib
 from ensure import ensure_annotations

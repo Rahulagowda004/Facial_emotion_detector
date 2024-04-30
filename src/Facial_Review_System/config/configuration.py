6e8fa2c8cdcf77be269f07fc2d003f8c86ca48ea
rahulagowda004 <rahulgowda277@gmail.com>
2024-04-30 15:28:02 +0530

    nothing

--- src/Facial_Review_System/config/configuration.py
+++ src/Facial_Review_System/config/configuration.py
@@ -59,7 +59,7 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "data")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "dataset")
         create_directories([
             Path(training.root_dir)
         ])
